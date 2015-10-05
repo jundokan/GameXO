644a2ea162f0a175affd645091bdd90b87ea7953
Sergii Kuzmin <jundokan2000@gmail.com>
2015-10-05 15:21:33 +0300

    Added MainGamePage file as  main View

---
+++ GameXO/GameXO/GameXO/ViewModels/MainGamePageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using GameXO.Droid.Annotations;
+
+namespace GameXO.ViewModels
+{
+    class MainGamePageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
